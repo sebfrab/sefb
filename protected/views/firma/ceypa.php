ecc481a436af05aa7eda062ea15385e5c8d3fd6b
Eninfosfranco <Eninfosfranco@Eninfosfranco.mare.cl>
2014-09-26 11:40:58 -0300

    ajuste firma ceypa

--- protected/views/firma/ceypa.php
+++ protected/views/firma/ceypa.php
@@ -1,4 +1,24 @@
+<style>
+    #author{
+        color: #2EFEF7;
+        text-decoration: none;
+        font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+        font-size: 10px;
+    }
+    
+    #author:hover{
+        color: #fff;
+    }
+    
+    #author>p{
+        display: inline;
+        text-align: right;
+    }
+</style>
 
-
-
-<a href="http://sefb.cl">SEFB - contacto@sefb.cl</a>
+<a id="author" href="http://sefb.cl" title="&#191; y por qu&eacute; no &#63; / contacto@sefb.cl">
+    <p>
+        ¿y por qué no? 
+        <p style="font-weight: bold; font-size: 12px;">Sebasti&aacute;n Franco</p>
+    </p>
+</a>
